6b9269c3a549dc390e91b7c9873deba7239e74fe
Paul Sender <paul@scalabledisplay.com>
2019-08-14 14:58:06 -0400

    1.2 Completed App

--- mern-todo-app/src/components/todos-list.component.js
+++ mern-todo-app/src/components/todos-list.component.js
@@ -1,77 +1,62 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+var Todo = props => (
+  <tr>
+    <td className={props.todo.todo_completed ? "completed" : ""}>
+      {props.todo.todo_description}
+    </td>
+    <td className={props.todo.todo_completed ? "completed" : ""}>
+      {props.todo.todo_responsible}
+    </td>
+    <td className={props.todo.todo_completed ? "completed" : ""}>
+      {props.todo.todo_priority}
+    </td>
+    <td>
+      <Link to={"/edit/" + props.todo._id}>Edit</Link>
+    </td>
+  </tr>
+);
 
 export default class TodosList extends Component {
   constructor(props) {
     super(props);
-
-    this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
-    this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this);
-    this.onChangeTodopriority = this.onChangeTodopriority.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-
-    this.state = {
-      todo_description: "",
-      todo_responsible: "",
-      todo_priority: "",
-      todo_completed: false
-    };
+    this.state = { todos: [] };
   }
 
-  onChangeTodoDescription(e) {
-    this.setState({
-      todo_description: e.target.value
+  componentDidMount() {
+    axios
+      .get("http://localhost:4000/todos/")
+      .then(response => {
+        this.setState({ todos: response.data });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+  todoList() {
+    return this.state.todos.map(function(currentTodo, i) {
+      return <Todo todo={currentTodo} key={i} />;
     });
   }
-  onChangeTodoResponsible(e) {
-    this.setState({
-      todo_responsible: e.target.value
-    });
-  }
-  onChangeTodopriority(e) {
-    this.setState({
-      todo_priority: e.target.value
-    });
-  }
-
-  onSubmit(e) {
-    e.preventDefault();
-
-    // Submit logic
-    console.log(`Form Submited:`);
-    console.log(`Todo Description: ${this.state.todo_description}`);
-    console.log(`Todo Responsible: ${this.state.todo_responsible}`);
-    console.log(`Todo Priority: ${this.state.todo_priority}`);
-    console.log(`Todo Completed: ${this.state.todo_completed}`);
-
-    this.setState({
-      todo_description: "",
-      todo_responsible: "",
-      todo_priority: "",
-      todo_completed: false
-    });
-  }
-  // onChangeTodoCompleted(e) {
-  //   this.setState({
-  //     todo_completed: e.target.value
-  //   });
-  //
-  // }
 
   render() {
     return (
-      <div style={{ marginTop: 20 }}>
-        <h3>Create New Todo</h3>
-        <form onSubmit={this.onSubmit}>
-          <div className="form-group">
-            <label>Description: </label>
-            <input
-              type="text"
-              className="form-control"
-              value={this.state.todo_description}
-              onChange={this.onChangeTodoDescription}
-            />
-          </div>
-        </form>
+      <div>
+        <h3>Todos List</h3>
+        <table className="table table-striped" style={{ marginTop: 20 }}>
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Responsible</th>
+              <th>Priority</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>{this.todoList()}</tbody>
+        </table>
       </div>
     );
   }
